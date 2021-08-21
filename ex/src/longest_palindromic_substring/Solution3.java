75a21651a5277e7dfc7ae62c22272cf02543b671
altsop <wdhmk0anjgithub7i>
2021-08-21 22:21:25 +0300

    - longest_palindromic_substring - palindrome_number

---
+++ ex/src/longest_palindromic_substring/Solution3.java
@@ -0,0 +1,32 @@
+package longest_palindromic_substring;
+
+public class Solution3 {
+
+    private int startIndex = 0;
+    private int longestPalindromeLength;
+
+    public String longestPalindrome(String s) {
+        if (s.length() < 2) {
+            return s;
+        }
+
+        for (int i = 0; i < s.length() - 1; i++) {
+            extendPalindrome(s, i, i);  //assume odd length, try to extend Palindrome as possible
+            extendPalindrome(s, i, i + 1); //assume even length.
+        }
+        return s.substring(startIndex, startIndex + longestPalindromeLength);
+    }
+
+    private void extendPalindrome(String s, int j, int k) {
+        // we expand in both directions
+        while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+            j--;
+            k++;
+        }
+        if (longestPalindromeLength < k - j - 1) {
+            startIndex = j + 1;
+            longestPalindromeLength = k - j - 1;
+        }
+    }
+
+}
