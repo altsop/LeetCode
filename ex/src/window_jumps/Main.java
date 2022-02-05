7ceb97e3344036b790a29cf0ee9b713dd6735653
Aleksei Tsõpov <aleksei.tsopov@ringit.ee>
2022-02-05 19:15:33 +0200

    - double solution for find low and high index keys - solution for window jumps

---
+++ ex/src/window_jumps/Main.java
@@ -0,0 +1,12 @@
+package window_jumps;
+
+import java.util.ArrayDeque;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayDeque<Integer> result = MaxSlidingWindow.findMaxSlidingWindow(new int[]{1,2,3, 4,5,6}, 3);
+        System.out.println(result);
+    }
+
+}
