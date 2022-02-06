e1926f5aaf0d22c95212532b8d5ca6f2ce5b65e1
Aleksei Tsõpov <aleksei.tsopov@ringit.ee>
2022-02-06 14:58:20 +0200

    - find sum of two

---
+++ ex/src/pair_with_given_sum/Solution.java
@@ -0,0 +1,32 @@
+package pair_with_given_sum;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+
+    static boolean findSumOfTwo(int[] array, int destinationSum) {
+        Set<Integer> remainders = new HashSet<>();
+
+        for (Integer element : array) {
+            int remainder = destinationSum - element;
+            if (remainders.contains(remainder)) {
+                return true;
+            }
+            remainders.add(remainder);
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] v = new int[]{ 5, 7, 1, 2, 8, 4, 3 };
+        int[] test = new int[]{ 3, 20, 1, 2, 7 };
+
+        for (int i = 0; i < test.length; i++) {
+            boolean output = findSumOfTwo(v, test[i]);
+            System.out.println("findSumOfTwo(v, " + test[i] + ") = " + (output ? "true" : "false"));
+        }
+
+    }
+}
