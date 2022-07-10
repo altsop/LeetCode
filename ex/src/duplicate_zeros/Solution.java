3f214443fce6c000f640327300d255583da415c3
aleksei.tsopov <aleksei.tsopov@ringit.ee>
2022-07-10 20:38:45 +0300

    duplicate_zeros merge_sorted_array

---
+++ ex/src/duplicate_zeros/Solution.java
@@ -0,0 +1,32 @@
+package duplicate_zeros;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    private static final Integer TARGET = 0;
+
+    public static void duplicateZeros(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int current = arr[i];
+            if (current == TARGET) {
+                int[] restOfArray = Arrays.copyOfRange(arr, i + 1, arr.length);
+                arr[i + 1] = TARGET;
+
+                for (int j = i + 2; j < arr.length; j++) {
+                    arr[j] = restOfArray[j - i - 2];
+                }
+
+                i++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] input = new int[]{1,0,2,3,0,4,5,0};
+        duplicateZeros(input);
+
+        System.out.println(Arrays.toString(input));
+    }
+
+}
---
+++ ex/src/merge_sorted_array/Solution.java
@@ -0,0 +1,46 @@
+package merge_sorted_array;
+
+import java.util.Arrays;
+
+public class Solution {
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        // 1. compare element from 1 with 2
+        // 2. shift if needed
+        // 3. fill the rest
+        int index1 = 0;
+        int index2 = 0;
+        while (index2 < n) {
+            int element1 = nums1[index1];
+            int element2 = nums2[index2];
+
+            if (element1 > element2) {
+                // save state before insertion
+                int[] shiftedPart = Arrays.copyOfRange(nums1, index1, m + n - 1);
+
+                // insert new
+                nums1[index1] = element2;
+
+                // insert shifted
+                for (int i = index1 + 1; i < m + n; i++) {
+                    nums1[i] = shiftedPart[i - index1 - 1];
+                }
+
+                index2++;
+            } else if (element1 == 0) {
+                nums1[index1] = element2;
+                index2++;
+            }
+
+            index1++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = new int[]{1,2,3,0,0,0};
+        merge(nums1, 3, new int[] {2,5,6}, 3);
+
+        System.out.println(Arrays.toString(nums1));
+    }
+
+}
