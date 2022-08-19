c56b2c4c33ce00061c08a25fe95b54fb0c529e8d
aleksei.tsopov <aleksei.tsopov@ringit.ee>
2022-08-19 11:59:14 +0300

    first_bad_version

---
+++ ex/src/first_bad_version/Solution.java
@@ -0,0 +1,30 @@
+package first_bad_version;
+
+public class Solution {
+
+    public static int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+
+        while (left < right) {
+            int pivot = left + (right - left) / 2;
+            if (isBadVersion(pivot)) {
+                right = pivot;
+            } else {
+                left = pivot + 1;
+            }
+        }
+
+        return left;
+    }
+
+    private static boolean isBadVersion(int n) {
+        return n == 1 || n == 2;
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(firstBadVersion(1));
+        System.out.println(firstBadVersion(4));
+    }
+
+}
