be4bfba1d2270bccef150280b32e2f557b87491a
aleksei.tsopov <aleksei.tsopov@ringit.ee>
2022-08-20 13:39:42 +0300

    validate_binary_search_tree

---
+++ ex/src/validate_binary_search_tree/Solution.java
@@ -0,0 +1,46 @@
+package validate_binary_search_tree;
+
+public class Solution {
+
+    public static boolean isValidBST(TreeNode root) {
+        return validate(root, null, null);
+    }
+
+    private static boolean validate(TreeNode root, Integer min, Integer max) {
+        if (root == null) {
+            return true;
+        }
+
+        if (max != null && root.val >= max || min != null && root.val <= min) {
+            return false;
+        }
+
+        return validate(root.left, min, root.val) && validate(root.right, root.val, max);
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+}
