dd1aedae5e80ac32e77627f24b8090f5e3a91d5a
aleksei.tsopov <aleksei.tsopov@ringit.ee>
2022-09-17 11:01:05 +0300

    find_the_index_of_the_first_occurrence_in_a_string

---
+++ ex/src/find_the_index_of_the_first_occurrence_in_a_string/Solution.java
@@ -0,0 +1,42 @@
+package find_the_index_of_the_first_occurrence_in_a_string;
+
+public class Solution {
+
+    private static final int MATCH_FLAG = -1;
+
+    public int strStr(String haystack, String needle) {
+        if (haystack.length() < needle.length()) {
+            return -1;
+        }
+
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            String substr = haystack.substring(i, i + needle.length());
+            int matchedChars = calculateAmountOfMatchedChars(substr, needle);
+            if (matchedChars == MATCH_FLAG) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    private int calculateAmountOfMatchedChars(String sample, String target) {
+        for (int i = 0; i < sample.length(); i++) {
+            char sampleChar = sample.charAt(i);
+            char targetChar = target.charAt(i);
+            if (sampleChar != targetChar) {
+                return i;
+            }
+        }
+
+        return MATCH_FLAG;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+//        System.out.println(solution.strStr("sadbutsad", "sad"));
+//        System.out.println(solution.strStr("leetcode", "leeto"));
+        System.out.println(solution.strStr("mississippi", "issip"));
+    }
+
+}
