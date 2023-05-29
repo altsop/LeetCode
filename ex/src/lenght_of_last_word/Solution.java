cd4010402c15e167b198bc7bc37429e077193c0b
Aleksei Tsypov <aleksei.tsypov@monese.com>
2023-05-29 19:43:47 +0300

    lenght_of_last_word

---
+++ ex/src/lenght_of_last_word/Solution.java
@@ -0,0 +1,46 @@
+package lenght_of_last_word;
+
+import java.util.Stack;
+
+//https://leetcode.com/problems/length-of-last-word/
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.lengthOfLastWord("   fly me   to   the moon  "));
+    }
+
+    public int lengthOfLastWord(String s) {
+        int startingIndex = findStartingIndex(s);
+        return readString(
+                s,
+                startingIndex
+        ).length();
+    }
+
+    private int findStartingIndex(String s) {
+        int cursor = s.length() - 1;
+
+        while (cursor >= 0 && s.charAt(cursor) == ' ') {
+            cursor--;
+        }
+
+        return cursor;
+    }
+
+    private String readString(String s, int cursor) {
+        Stack<Character> stack = new Stack<>();
+        while (cursor >= 0 && s.charAt(cursor) != ' ') {
+            stack.add(s.charAt(cursor));
+            cursor--;
+        }
+
+        StringBuilder builder = new StringBuilder();
+        while (!stack.isEmpty()) {
+            builder.append(stack.pop());
+        }
+
+        return builder.toString();
+    }
+
+}
