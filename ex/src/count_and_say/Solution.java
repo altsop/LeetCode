7ffcca904b0535836cd571a4f5426322404651d2
Aleksei Tsypov <aleksei.tsypov@monese.com>
2023-06-05 17:31:22 +0300

    combination_sum

--- ex/src/count_and_say/Solution.java
+++ ex/src/count_and_say/Solution.java
@@ -26,38 +26,7 @@ public class Solution {
                 }
                 amount++;
             }
-            private static long[] MEMORY = new long[30];
 
-            static {
-                MEMORY[0] = 1;
-            }
-
-            public String countAndSay(int n) {
-                if (MEMORY[n - 1] != 0) {
-                    return String.valueOf(MEMORY[n - 1]);
-                }
-
-                String previous = countAndSay(n - 1);
-
-                StringBuilder builder = new StringBuilder();
-                for (int i = 0; i < previous.length(); i++) {
-                    char current = previous.charAt(i);
-                    int amount = 1;
-                    for (int j = i + 1; j < previous.length(); j++) {
-                        int next = previous.charAt(j);
-                        if (current != next) {
-                            break;
-                        }
-                        amount++;
-                    }
-
-                    i += amount - 1;
-                    builder.append(amount).append(current);
-                }
-
-//        MEMORY[n - 1] = Long.parseLong(builder.toString());
-                return builder.toString();
-            }
             i += amount - 1;
             builder.append(amount).append(current);
         }
